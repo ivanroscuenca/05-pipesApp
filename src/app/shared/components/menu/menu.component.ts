d6d69244ba177b50e76a22af9c1740bd72b77f6f
ivanroscuenca <ivanros@protonmail.com>
2023-08-16 11:50:02 +0200

    video150Modification

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -7,7 +7,6 @@ import { MenuItem } from 'primeng/api';
 })
 export class MenuComponent implements OnInit {
   public menuItems: MenuItem[] | undefined;
-
   ngOnInit() {
     this.menuItems = [
       {
