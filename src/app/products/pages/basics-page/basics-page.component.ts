a301388d03a1c5a16d4af35cf31cec90b7568d9a
ivanroscuenca <ivanros@protonmail.com>
2023-08-17 22:29:56 +0200

    video153

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrls: ['./basics-page.component.css']
+  styleUrls: ['./basics-page.component.css'],
 })
 export class BasicsPageComponent {
-
+  public nameLower: string = 'ivan';
+  public nameUpper: string = 'IVAN';
+  public fullname: string = 'iVaN rOs';
 }
